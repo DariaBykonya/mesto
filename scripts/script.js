a0d10f35631802b4d16ffa533317e35da2645437
Daria Bykonya <dashabykonya@yandex.ru>
2023-02-24 18:32:47 +0300

    fix: script

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,15 @@ editProfileButton.addEventListener('click', function () {
   editPopup.classList.add('popup_opened');
 });
 
-let buttonCloseEdit = document.querySelector('.popup__close');
-buttonCloseEdit.addEventListener('click', function () {
-  editPopup.classList.remove('popup_opened');
-});
-
 // Находим форму в DOM
 let formElement = document.querySelector('.popup'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__inpu_type_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__input_type_job');
+let nameInput = document.querySelector('.popup__input_type_name').value; // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_type_job').value;
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 let rename = document.querySelector('.profile__name');
-let editJob = document.querySelector('.profile__text');
+let editJob = document.querySelector('.profile__text').value;
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
@@ -29,6 +24,10 @@ function handleFormSubmit(evt) {
   rename.textContent = nameInput;
   editJob.textContent = jobInput;
 }
+let buttonCloseEdit = document.querySelector('.popup__close');
+buttonCloseEdit.addEventListener('click', function () {
+  editPopup.classList.remove('popup_opened');
+});
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
