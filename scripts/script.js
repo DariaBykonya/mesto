eded9f1e4979f4ea36826009635c699937ba25ec
Daria Bykonya <dashabykonya@yandex.ru>
2023-02-26 20:14:01 +0300

    Fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,42 @@
 let editProfileButton = document.querySelector('.profile__edit-button');
 let editPopup = document.querySelector('.popup');
+let formElement = document.querySelector('#edit');
+
+let currentName = document.querySelector('.profile__name');
+
+let nameInput = document.querySelector('.popup__input_type_name');
+
+let jobInput = document.querySelector('.popup__input_type_job');
+
+let currentJob = document.querySelector('.profile__text');
 editProfileButton.addEventListener('click', function () {
-  editPopup.classList.add('popup_opened');
+  openPopup(editPopup);
+  nameInput.value = currentName.textContent;
+  jobInput.value = currentJob.textContent;
 });
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_type_name').value; // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__input_type_job').value;
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-let rename = document.querySelector('.profile__name');
-let editJob = document.querySelector('.profile__text').value;
-function handleFormSubmit(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+let buttonSaveEditForm = document.querySelector('.popup__save');
 
-  // Получите значение полей jobInput и nameInput из свойства value
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // Вставьте новые значения с помощью textContent
-  rename.textContent = nameInput;
-  editJob.textContent = jobInput;
+let buttonCloseEditForm = document.querySelector('.popup__close');
+buttonCloseEditForm.addEventListener('click', function () {
+  closePopup(editPopup);
+});
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
-let buttonCloseEdit = document.querySelector('.popup__close');
-buttonCloseEdit.addEventListener('click', function () {
-  editPopup.classList.remove('popup_opened');
-});
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  currentName.textContent = nameInput.value;
+  currentJob.textContent = jobInput.value;
+
+  closePopup(editPopup);
+}
+
+buttonSaveEditForm.addEventListener('click', handleFormSubmit);
