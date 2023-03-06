0337b983ec6d8220f05d2f17aeebbac85037aa6c
Daria Bykonya <dashabykonya@yandex.ru>
2023-03-07 00:24:30 +0300

    feat: Добавить карточки на страницу через JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+const editCardsPopup = document.querySelector('#cardsPopup');
+const addMestoPopup = document.querySelector('#mestoPopup');
 let editProfileButton = document.querySelector('.profile__edit-button');
-let editPopup = document.querySelector('.popup');
+
 let formElement = document.querySelector('#edit');
 
 let currentName = document.querySelector('.profile__name');
@@ -9,18 +11,21 @@ let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
 
 let currentJob = document.querySelector('.profile__text');
+
 editProfileButton.addEventListener('click', function () {
-  openPopup(editPopup);
+  openPopup(editCardsPopup);
+  // openPopup(addMestoPopup);
   nameInput.value = currentName.textContent;
   jobInput.value = currentJob.textContent;
 });
 
-let buttonSaveEditForm = document.querySelector('.popup__save');
+let buttonSaveEditForm = editCardsPopup.querySelector('.popup__save');
 
-let buttonCloseEditForm = document.querySelector('.popup__close');
+let buttonCloseEditForm = editCardsPopup.querySelector('.popup__close');
 buttonCloseEditForm.addEventListener('click', function () {
-  closePopup(editPopup);
+  closePopup(editCardsPopup);
 });
+const buttonAddMesto = document.querySelector('.pop');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -36,7 +41,85 @@ function handleFormSubmit(evt) {
   currentName.textContent = nameInput.value;
   currentJob.textContent = jobInput.value;
 
-  closePopup(editPopup);
+  closePopup(editCardsPopup);
 }
-
 buttonSaveEditForm.addEventListener('click', handleFormSubmit);
+
+const addMestoButton = document.querySelector('.profile__add-button');
+const buttonCreateMesto = addMestoPopup.querySelector('.popup__save');
+const closeAddPopup = addMestoPopup.querySelector('.popup__close');
+
+addMestoButton.addEventListener('click', function () {
+  openPopup(addMestoPopup);
+});
+
+closeAddPopup.addEventListener('click', function () {
+  closePopup(addMestoPopup);
+});
+
+// function handleFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   currentName.textContent = nameInput.value;
+//   currentJob.textContent = jobInput.value;
+
+//   closePopup(editCardsPopup);
+// }
+// buttonSaveEditForm.addEventListener('click', handleFormSubmit);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  closePopup(addMestoPopup);
+}
+buttonCreateMesto.addEventListener('click', handleFormSubmit);
+
+const cards = [
+  {
+    name: 'Архыз',
+    alt: '',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    alt: '',
+    image:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    alt: '',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    alt: '',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    alt: '',
+    image:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    alt: '',
+    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const addCardsMesto = document.querySelector('.elements');
+
+const createCard = card => {
+  const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
+  const cardName = newCard.querySelector('.element__text');
+
+  cardName.textContent = card.name;
+
+  const cardImage = newCard.querySelector('.element__image');
+
+  cardImage.setAttribute('src', card.image);
+  cardImage.setAttribute('alt', card.alt);
+  addCardsMesto.append(newCard);
+};
