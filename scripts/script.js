e03c7e373c8df93999c73036b45e0bc29c7dd9cb
Daria Bykonya <dashabykonya@yandex.ru>
2023-03-09 21:00:34 +0300

    fix: изменены изображения в массиве

--- scripts/script.js
+++ scripts/script.js
@@ -82,13 +82,14 @@ const cards = [
   {
     name: 'Архыз',
     alt: '',
-    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    image:
+      'https://images.unsplash.com/photo-1638989280415-d58f7340b273?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
   },
   {
     name: 'Челябинская область',
     alt: '',
     image:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      'https://images.unsplash.com/photo-1575738171526-df0217663296?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
   },
   {
     name: 'Иваново',
@@ -98,7 +99,8 @@ const cards = [
   {
     name: 'Камчатка',
     alt: '',
-    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    image:
+      'https://images.unsplash.com/photo-1665073140507-0bad3d962476?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1203&q=80'
   },
   {
     name: 'Холмогорский район',
@@ -109,7 +111,8 @@ const cards = [
   {
     name: 'Байкал',
     alt: '',
-    image: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    image:
+      'https://images.unsplash.com/photo-1551845041-63e8e76836ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80'
   }
 ];
 
