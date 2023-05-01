ac278b618acfafc0d98ec70a9283f10db6b71de0
Daria Bykonya <dashabykonya@yandex.ru>
2023-05-01 23:11:17 +0300

    add class Card and FormValidator

---
+++ scripts/Card.js
@@ -0,0 +1,54 @@
+// создание класса Card, который создаёт карточку с текстом и ссылкой на изображение
+export default class Card {
+  constructor(data, templateSelector, openImagePopup) {
+    this._data = data;
+    this._name = data.name;
+    this._image = data.image;
+    this._templateSelector = templateSelector;
+    this._openImagePopup = openImagePopup;
+  }
+
+  _getTepmlate() {
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector('.element')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  // добавление лайка на карточке
+  _addLikeCard = () => {
+    this._likeButtonElement.classList.toggle('element__like-button_active');
+  };
+
+  // удаление карточки
+  _deleteCard = () => {
+    this._element.remove();
+  };
+
+  _handleOpenImagePopup = () => {
+    this._openImagePopup(this._data);
+  };
+
+  _setEventListeners() {
+    this._likeButtonElement.addEventListener('click', this._addLikeCard);
+    this._deleteButtonElement.addEventListener('click', this._deleteCard);
+    this._imageElement.addEventListener('click', this._handleOpenImagePopup);
+  }
+
+  createCard() {
+    this._element = this._getTepmlate();
+    this._imageElement = this._element.querySelector('.element__image');
+    this._likeButtonElement = this._element.querySelector('.element__like-button');
+    this._deleteButtonElement = this._element.querySelector('.element__delete-button');
+    this._textElement = this._element.querySelector('.element__text');
+
+    this._imageElement.src = this._image;
+    this._imageElement.alt = this._name;
+    this._textElement.textContent = this._name;
+
+    this._setEventListeners();
+    return this._element;
+  }
+}
---
+++ scripts/FormValidator.js
@@ -0,0 +1,89 @@
+export default class FormValidator {
+  constructor(config, form) {
+    this._inputSelector = config.inputSelector;
+    this._errorSelectorTemplate = config.errorSelectorTemplate;
+    this._activeErrorClass = config.activeErrorClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._activeButtonClass = config.activeButtonClass;
+    this._form = form;
+    this._button = form.querySelector(this._submitButtonSelector);
+    this._inputList = form.querySelectorAll(this._inputSelector);
+  }
+
+  _showInputError() {
+    this._input.classList.add(this._inputErrorClass);
+    this._errorTextElement.textContent = this._input.validationMessage;
+    this._errorTextElement.classList.add(this._activeErrorClass);
+  }
+
+  _hideInputError() {
+    this._input.classList.remove(this._inputErrorClass);
+    this._errorTextElement.classList.remove(this._activeErrorClass);
+    this._errorTextElement.textContent = '';
+  }
+
+  _enableButton() {
+    this._button.classList.remove(this._inactiveButtonClass);
+    this._button.classList.add(this._activeButtonClass);
+    this._button.disabled = false;
+  }
+
+  _disableButton() {
+    this._button.classList.add(this._inactiveButtonClass);
+    this._button.classList.remove(this._activeButtonClass);
+    this._button.disabled = true;
+  }
+
+  _hasInvalidInput() {
+    return Array.from(this._inputList).some(input => !input.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (!this._hasInvalidInput()) {
+      this._enableButton();
+    } else {
+      this._disableButton();
+    }
+  }
+
+  _checkInputValidity(input) {
+    this._errorTextElement = this._form.querySelector(
+      `${this._errorSelectorTemplate}${this._input.id}`
+    );
+    if (!this._input.validity.valid) {
+      this._showInputError();
+    } else {
+      this._hideInputError();
+    }
+  }
+
+  _setEventListener() {
+    this._inputList.forEach(input => {
+      input.addEventListener('input', () => {
+        this._input = input;
+        console.log(this);
+        this._checkInputValidity();
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._setEventListener();
+  }
+
+  resetErrorPopup() {
+    this._inputList.forEach(input => {
+      this._input = input;
+      this._errorTextElement = this._form.querySelector(
+        `${this._errorSelectorTemplate}${this._input.id}`
+      );
+      if (!input.validity.valid) {
+        this._hideInputError();
+      }
+    });
+    this._disableButton();
+  }
+}
